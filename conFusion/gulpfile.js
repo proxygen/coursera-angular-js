f21276aeddbf236cdf927a23b5c5e79a2fa2df1e
proxygen <xiaodong.yang3@gmail.com>
2015-12-22 03:08:19 +0100

    Added comment form on dishdetail page

--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/dishdetail.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
@@ -77,7 +77,7 @@ gulp.task('browser-sync', ['default'], function () {
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
+         index: "dishdetail.html"
       }
    });
         // Watch any files in dist/, reload on change
